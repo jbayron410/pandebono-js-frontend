775e326bac6fc5bfa7a1abce3cc5bc4c17100562
John Castaneda <bayron410@gmail.com>
2026-04-06 20:36:15 -0500

    feat:MyBoooky-1 HU-boiler-plate estructura

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,17 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: '',
-    loadComponent: () => import('./features/home/home').then(m => m.Home)
-  }
+    loadComponent: () => import('./core/main-layout/main-layout').then((m) => m.MainLayout),
+    children: [
+      {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full',
+      },
+      {
+        path: 'home',
+        loadComponent: () => import('./features/home/home').then((m) => m.Home),
+      },
+    ],
+  },
 ];
